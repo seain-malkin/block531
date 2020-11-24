422db2da80e3d71f62ae4f3bc76eb3065b61a7b6
Seain Malkin <seainmalkin@gmail.com>
2020-11-24 17:09:26 +0800

    Added bottom menu and toolbar

--- app/src/main/java/com/example/block531/PlanActivity.kt
+++ app/src/main/java/com/example/block531/PlanActivity.kt
@@ -4,13 +4,17 @@ package com.example.block531
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.block531.fragments.BottomMenu
 
 class PlanActivity : AppCompatActivity() {
 
+    private lateinit var bottomMenu: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         // Enable night theme
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -19,7 +23,8 @@ class PlanActivity : AppCompatActivity() {
         supportFragmentManager.let {fm ->
             // Create each fragment, reference it, and add it to the transaction
             fm.beginTransaction().run {
-
+                bottomMenu = BottomMenu.newInstance()
+                add(R.id.frame_bottom_menu, bottomMenu)
 
                 // Run the transaction
                 commit()
