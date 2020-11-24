c50c7153910312d95b5977c892dc2b7591eb14d9
Seain Malkin <seainmalkin@gmail.com>
2020-11-24 15:43:52 +0800

    Removed ticks fragment_ticks.xml

--- app/src/main/java/com/example/block531/PlanActivity.kt
+++ app/src/main/java/com/example/block531/PlanActivity.kt
@@ -4,12 +4,9 @@ package com.example.block531
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
-import com.example.block531.fragments.TicksFragment
 
 class PlanActivity : AppCompatActivity() {
 
-    /**  Reference to the [TicksFragment] object */
-    private lateinit var ticksFragment: TicksFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +19,7 @@ class PlanActivity : AppCompatActivity() {
         supportFragmentManager.let {fm ->
             // Create each fragment, reference it, and add it to the transaction
             fm.beginTransaction().run {
-                // Ticks Fragment setup
-                ticksFragment = TicksFragment.newInstance(1, 1, 1)
-                add(R.id.fragmentTicks, ticksFragment)
+
 
                 // Run the transaction
                 commit()
